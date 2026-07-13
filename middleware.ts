146fc4b56e63205e48c0bdffff4719841192ae63
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-07-13 18:29:51 +0600

    Update the middleware conditions for pending role status

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,7 @@ export async function middleware(req: NextRequest) {
 
     const isPublic = PUBLIC_PATHS.includes(pathname);
     const isOnboarding = pathname.startsWith("/onboarding");
+    const isPending = pathname.startsWith("/pending");
 
     // if user is not logged in
     if(!token) {
@@ -16,14 +17,30 @@ export async function middleware(req: NextRequest) {
         return NextResponse.redirect(new URL("/login", req.url))
     }
 
-    // user is logged in but has no role -> onboarding
-    if(!token.role && !isOnboarding) {
-        return NextResponse.redirect(new URL("/onboarding", req.url))
-    }  
+    const roleStatus = token.roleStatus ?? "unset";
 
-    // user is logged in and has specific role then redirect to dashboard layout
-    if(token.role && isOnboarding) {
-        return NextResponse.redirect(new URL("/dashboard", req.url))
+    // user is logged in but has no role -> onboarding
+    if (roleStatus === "unset") {
+        if (!isOnboarding) {
+        return NextResponse.redirect(new URL("/onboarding", req.url));
+        }
+        return NextResponse.next();
+    }
+
+    // user logged in and select the role now waiting for approval
+    if (roleStatus === "pending" || roleStatus === "rejected") {
+        if (!isPending) {
+        return NextResponse.redirect(new URL("/pending", req.url));
+        }
+        return NextResponse.next();
+    }
+
+    // user is logged in and has specific role, also get the approval then redirect to dashboard layout
+    if (roleStatus === "approved") {
+        if (isOnboarding || isPending) {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+        }
+        return NextResponse.next();
     }
 
     return NextResponse.next();
