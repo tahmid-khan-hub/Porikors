744bd7c4eeb47f1fcc53ee5e35577e33af1fe120
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-07-30 23:34:09 +0600

    Create the TeacherChrome component, structure the teacher layout

--- app/(teacher)/layout.tsx
+++ app/(teacher)/layout.tsx
@@ -1,3 +1,14 @@
+import TeacherChrome from "@/components/teacher/TeacherChrome";
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
 export default async function TeacherLayout({ children }: { children: React.ReactNode }) {
+    const session = await getServerSession(authOptions);
+
+    if (!session?.user) redirect("/login");
+
+    if(session.user.role !== "teacher" || session.user.roleStatus !== "approved") redirect("/404");
     
+    return <TeacherChrome user={session.user}>{children}</TeacherChrome>;
 }
---
+++ components/teacher/TeacherChrome.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+interface TeacherChromeProps {
+    user: { name?: string | null; email?: string | null; image?: string | null },
+    title?: string;
+    children: React.ReactNode;
+}
+
+export default function TeacherChrome({ user, title, children } : TeacherChromeProps) {
+    return (
+        <div>
+
+        </div>
+    )
+}
