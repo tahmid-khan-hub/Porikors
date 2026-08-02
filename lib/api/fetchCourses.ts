75966313ac8b5a1daa7c6667cc800cf8bc73e35d
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-08-02 21:04:28 +0600

    Create the fetchCourseDetail inside lib

--- lib/api/fetchCourses.ts
+++ lib/api/fetchCourses.ts
@@ -1,8 +1,15 @@
-import { Course } from "@/types/course";
+import { Course, CourseDetail } from "@/types/course";
 
 export async function fetchTeacherCourses(): Promise<Course[]> {
     const res = await fetch("/api/teacher/courses");
     if (!res.ok) throw new Error("Failed to fetch courses");
     const data = await res.json();
     return data.courses;
+}
+
+export async function fetchCourseDetail(id: string): Promise<CourseDetail> {
+    const res = await fetch(`/api/teacher/courses/${id}`)
+    if(!res.ok) throw new Error("Failed to fetch course");
+    const data = await res.json();
+    return data.course;
 }
