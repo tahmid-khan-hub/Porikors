91f4fc46ef93a64e66aab3ce2fcbb535de01ca39
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-08-04 00:07:18 +0600

    Create the resources page for courses

---
+++ app/(teacher)/teacher/courses/[id]/resources/page.tsx
@@ -0,0 +1,11 @@
+import CourseResourcesGrid from "@/components/teacher/courses/coursesDetails/CourseResourcesGrid";
+
+export default async function CourseResourcesPage({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+
+    return (
+        <div>
+            <CourseResourcesGrid courseId={id} />;
+        </div>
+    )
+}
